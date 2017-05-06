3e6f8b9b5c0594369118606764d13f05e45271d0
Marcin Kacperski <mkac33@gmail.com>
2017-05-06 22:16:24 +0200

    added GameplayScreen

---
+++ core/src/com/mygdx/game/screens/GameplayScreen.java
@@ -0,0 +1,21 @@
+package com.mygdx.game.screens;
+
+import com.mygdx.game.MyGdxGame;
+
+/**
+ * Created by Ja on 2017-05-06.
+ */
+
+public class GameplayScreen extends AbstractScreen{
+
+    public GameplayScreen(MyGdxGame game) {
+        super(game);
+        init();
+        
+    }
+
+    private void init() {
+    }
+
+
+}
