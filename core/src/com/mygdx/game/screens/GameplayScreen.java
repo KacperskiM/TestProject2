b253fb8e56ac5e03be4eec6cf191fa8da0ac38bd
Marcin Kacperski <mkac33@gmail.com>
2017-05-06 22:48:01 +0200

    added player

--- core/src/com/mygdx/game/screens/GameplayScreen.java
+++ core/src/com/mygdx/game/screens/GameplayScreen.java
@@ -1,12 +1,15 @@
 package com.mygdx.game.screens;
 
 import com.mygdx.game.MyGdxGame;
+import com.mygdx.game.entities.Player;
 
 /**
  * Created by Ja on 2017-05-06.
  */
 
 public class GameplayScreen extends AbstractScreen{
+
+    private Player player;
 
     public GameplayScreen(MyGdxGame game) {
         super(game);
@@ -15,8 +18,25 @@ public class GameplayScreen extends AbstractScreen{
     }
 
     private void init() {
+        initPlayer();
+    }
+
+    private void initPlayer() {
+        player = new Player();
+        stage.addActor(player);
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+        update();
+        spriteBatch.begin();
+        stage.draw();
+        spriteBatch.end();
 
     }
 
-
+    private void update() {
+        stage.act();
+    }
 }
