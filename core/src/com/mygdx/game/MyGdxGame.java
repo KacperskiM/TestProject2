f0788127bf360c2bb84102a6ce26dea9afd5c497
Marcin Kacperski <mkac33@gmail.com>
2017-05-10 15:50:00 +0200

    saving/loading playerScore

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,7 +1,8 @@
 package com.mygdx.game;
 
-
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
 import com.mygdx.game.screens.SplashScreen;
 
 
@@ -10,13 +11,20 @@ public class MyGdxGame extends Game {
 	public final static int WIDTH = 1280;
 	public final static int HEIGHT = 720;
 
+    public final static String GAME_PREFS = "com.mygdx.game.prefs";
+    public final static String GAME_SCORE = "com.mygdx.game.prefs.score";
+
 	private boolean paused;
 
 	private int points;
 
+    private Preferences prefs;
+
 
 	public void addPoint(){
 		points++;
+        prefs.putInteger(GAME_SCORE,points);
+        prefs.flush();
 	}
 
 
@@ -27,11 +35,21 @@ public class MyGdxGame extends Game {
 
 	@Override
 	public void create(){
-		this.setScreen(new SplashScreen(this));
+		init();
+        this.setScreen(new SplashScreen(this));
 }
 
+    private void init() {
+        prefs = Gdx.app.getPreferences(GAME_PREFS);
+        loadScore();
+    }
 
-	public boolean isPaused() {
+    private void loadScore() {
+        points = prefs.getInteger(GAME_SCORE);
+    }
+
+
+    public boolean isPaused() {
 		return paused;
 	}
 
